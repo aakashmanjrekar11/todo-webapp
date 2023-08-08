bbccd6ac4f2ed7b74bf76bde10cebb6a09d57801
Aakash Manjrekar <aakashmanjrekar@gmail.com>
2023-08-08 03:20:52 -0400

    models/user.js -> defined DB user-schema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+//! 4. Defininig the database schema for the user
+const mongoose = require("mongoose");
+
+// create user schema
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		min: 3,
+		max: 255,
+	},
+	email: {
+		type: String,
+		required: true,
+		min: 6,
+		max: 255,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		min: 6,
+		max: 1024,
+	},
+});
+
+// export user model
+const User = mongoose.model("User", userSchema);
+module.exports = User;
