cdb7d01e2cb0e987926e5f589662ea5773aed3cf
Aakash Manjrekar <aakashmanjrekar@gmail.com>
2023-08-15 22:57:16 -0400

    ⚙ PUT now protected

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const verifyToken = require("../verifyToken"); // import verifyToken middleware
 const Task = require("../models/Task"); // import Task model from models/Task.js
 
-// Route for creating a new task
+//* Route for creating a new task
 router.post("/tasks", verifyToken, async (req, res) => {
 	try {
 		const userId = req.user; // Get the user's ID from the authenticated request
+
 		const taskData = {
 			title: req.body.title,
 			description: req.body.description,
@@ -17,45 +18,72 @@ router.post("/tasks", verifyToken, async (req, res) => {
 
 		const task = new Task(taskData);
 
-		// const task = new Task(req.body); // create a new task with request body
 		await task.save(); // save the task to database
+
 		res.status(201).json({ message: "Task created successfully", task });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
 	}
 });
 
-// Route for fetching all tasks
+//* Route for fetching all tasks
 router.get("/tasks", verifyToken, async (req, res) => {
 	try {
 		const userId = req.user; // Get the user's ID from the authenticated request
+
 		const tasks = await Task.find({ userId }); // Filter tasks based on the user's ID
-		// const tasks = await Task.find(); // fetch all tasks from database
+
 		res.status(200).json(tasks);
 	} catch (error) {
 		res.status(500).json({ error: error.message });
 	}
 });
 
-// Route for updating a task by id
+//* Route for updating a task by id
 router.put("/tasks/:id", verifyToken, async (req, res) => {
 	try {
-		const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-		}); // find task by id and update it with request body
+		const userId = req.user; // Get the user's ID from the authenticated request
+
+		const task = await Task.findById(req.params.id); // Find the task by id
+
 		if (!task) {
 			return res.status(404).json({ error: "Task not found" });
 		}
+
+		// Check if the task belongs to the authenticated user
+		if (task.userId.toString() !== userId) {
+			return res
+				.status(403)
+				.json({ error: "You do not have permission to update this task" });
+		}
+
+		// Update the task with the request body data
+		task.title = req.body.title || task.title;
+		task.description = req.body.description || task.description;
+		task.completed = req.body.completed || task.completed;
+
+		await task.save();
+
 		res.status(200).json({
 			message: "Task updated successfully",
 			data: task,
 		});
+		// const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+		// 	new: true,
+		// }); // find task by id and update it with request body
+		// if (!task) {
+		// 	return res.status(404).json({ error: "Task not found" });
+		// }
+		// res.status(200).json({
+		// 	message: "Task updated successfully",
+		// 	data: task,
+		// });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
 	}
 });
 
-// Route for deleting a task by id
+//* Route for deleting a task by id
 router.delete("/tasks/:id", verifyToken, async (req, res) => {
 	try {
 		const task = await Task.findByIdAndDelete(req.params.id); // find task by id and delete it
