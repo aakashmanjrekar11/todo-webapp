028de9c3826f9d707f5f8e54963e3c0f2afe14d9
Aakash Manjrekar <aakashmanjrekar@gmail.com>
2023-08-15 21:59:38 -0400

    verifyToken success, login verification disabled for testing

--- verifyToken.js
+++ verifyToken.js
@@ -4,14 +4,17 @@ const jwt = require("jsonwebtoken"); // import jsonwebtoken
 const verifyToken = (req, res, next) => {
 	const token = req.header("Authorization"); // get token from header
 	if (!token) {
-		return res.status(401).json({ error: "Access denied. Token missing." }); // if token is missing, return error
+		//todo - disabling verifyToken for now - need to figure out how to get token from header
+		// return res.status(401).json({ error: "Access denied. Token missing." }); // if token is missing, return error
+		return next(); //todo testing, remove return next(); later
 	}
 
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); // verify token
-		req.user = decoded.user; // add user from payload to request object
+		req.user = decoded.userId; // add user from payload to request object
 		next(); // call next middleware function
 	} catch (error) {
+		console.error("Token verification error:", error);
 		res.status(401).json({ error: "Invalid token." }); // if token is invalid, return error
 	}
 };
